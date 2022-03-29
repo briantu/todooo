f6236710059e0ec5e52eca466c876067bc121291
briantu <brianptu2@gmail.com>
2022-03-29 13:53:47 -0400

    Optimize background

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -5,6 +5,13 @@ import useWindowSize from "../utils/useWindowSize";
 
 const Parallax = require("parallax-js");
 
+const ROTATE_DEGREE = 25;
+const ICON_WIDTH = 40;
+const ICON_SPACING = 30;
+const ICON_DISTANCE = ICON_WIDTH + ICON_SPACING;
+const ICON_TOP_OFFSET =
+  ICON_DISTANCE * Math.tan((ROTATE_DEGREE * Math.PI) / 180);
+
 const icons = [
   ["among-us", "binoculars", "bitcoin", "bookmark"],
   ["calculator", "contacts", "discord", "fl-studio"],
@@ -20,26 +27,37 @@ const Background = () => {
   });
 
   return (
-    <Box id="scene" position="absolute" left="-100px" top="-400px">
-      <VStack data-depth="0.1" spacing="30px">
-        {[...Array(Math.floor(size.height / 70 / 4) + 4)].map(() => {
-          return icons.map((iconList) => {
-            return (
-              <HStack spacing="30px" transform="rotate(-25deg)">
-                {[...Array(Math.floor(size.width / 70 / 4) + 2)].map(() => {
-                  return iconList.map((icon) => (
-                    <Image
-                      src={`/icons/icons8-${icon}.svg`}
-                      h="40px"
-                      className={styles.unselectable}
-                    />
-                  ));
-                })}
-              </HStack>
-            );
-          });
-        })}
-      </VStack>
+    <Box id="scene" w="full" position="absolute">
+      <Box data-depth="0.1">
+        <HStack spacing={`${ICON_SPACING}px`}>
+          {[...Array(Math.floor(size.width / ICON_DISTANCE / 4) + 1)].map(
+            () => {
+              return icons.map((iconList, i) => {
+                return (
+                  <VStack
+                    spacing={`${ICON_SPACING}px`}
+                    position="relative"
+                    top={`-${i * ICON_TOP_OFFSET}px`}
+                  >
+                    {[
+                      ...Array(Math.floor(size.height / ICON_DISTANCE / 4) + 1),
+                    ].map(() => {
+                      return iconList.map((icon) => (
+                        <Image
+                          src={`/icons/icons8-${icon}.svg`}
+                          h={`${ICON_WIDTH}px`}
+                          className={styles.unselectable}
+                          transform={`rotate(-${ROTATE_DEGREE}deg)`}
+                        />
+                      ));
+                    })}
+                  </VStack>
+                );
+              });
+            }
+          )}
+        </HStack>
+      </Box>
     </Box>
   );
 };
