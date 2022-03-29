74b1ea45fdaffd5f0476f60d9c3874eef982e561
briantu <brianptu2@gmail.com>
2022-03-29 01:47:09 -0400

    Animate after load

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
 import { VStack, Text, Progress, Box, Divider } from "@chakra-ui/react";
 import styles from "../styles/Category.module.css";
 
 const Category = () => {
+  const [progressClassName, setProgressClassName] = useState("");
+  const [progressValue, setProgressValue] = useState(0);
+
+  // This will run one time after the component mounts
+  useEffect(() => {
+    const onPageLoad = () => {
+      setProgressClassName(styles.progress);
+      setProgressValue(60);
+    };
+
+    // Check if the page has already loaded
+    if (document.readyState === "complete") {
+      onPageLoad();
+    } else {
+      window.addEventListener("load", onPageLoad);
+      // Remove the event listener when component unmounts
+      return () => window.removeEventListener("load", onPageLoad);
+    }
+  }, []);
+
   return (
     <VStack
       px={5}
@@ -20,22 +41,25 @@ const Category = () => {
       <Box w="full" bg="transparent">
         <Box w="full" bg="gray.200" borderRadius="md">
           <Progress
-            value={60}
+            value={progressValue}
             w="full"
             size="xs"
             bg="gray.200"
             borderRadius="md"
             variant="pink"
-            className={styles.progress}
+            className={progressClassName}
           />
         </Box>
-        <Box w="calc(60% - 4px)" bg="transparent">
+        <Box
+          w={!progressValue ? 0 : `calc(${progressValue}% - 4px)`}
+          bg="transparent"
+        >
           <Divider
             h={0}
             bg="transparent"
             borderRadius="md"
             boxShadow="2px 0px 10px 2px rgba(218, 0, 230, 0.5)"
-            className={styles.progress}
+            className={progressClassName}
           />
         </Box>
       </Box>
