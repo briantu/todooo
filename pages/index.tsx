d67f3e93020e0ecdb98ca9db8f61b6c2c1eda22e
briantu <brianptu2@gmail.com>
2022-04-05 20:24:08 -0400

    Add head to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,25 @@
+import type { NextPage } from "next";
+import Head from "next/head";
 import { Flex } from "@chakra-ui/react";
 import Todooo from "../src/components/Todooo";
 import Background from "../src/components/Background";
 
-const IndexPage = () => {
+const IndexPage: NextPage = () => {
   return (
-    <Flex position="relative" justifyContent="center">
-      <Background />
-      <Todooo />
-    </Flex>
+    <>
+      <Head>
+        <title>Todooo</title>
+        <meta
+          name="description"
+          content="The only todo list you will ever need"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Flex position="relative" justifyContent="center">
+        <Background />
+        <Todooo />
+      </Flex>
+    </>
   );
 };
 
